8d1e69c2b5332653f3945ba6aa1aa570e6d6b82a
ydj515 <ydj515@hanmail.net>
2019-07-31 02:11:40 +0900

    prototype commit

---
+++ src/prototype_pattern/BlueColor.java
@@ -0,0 +1,8 @@
+package prototype_pattern;
+
+public class BlueColor extends Color {
+	
+	public BlueColor() {
+		this.colorName = "blue";
+	}
+}
---
+++ src/prototype_pattern/RedColor.java
@@ -0,0 +1,8 @@
+package prototype_pattern;
+
+public class RedColor extends Color {
+	
+	public RedColor() {
+		this.colorName = "red";
+	}
+}
