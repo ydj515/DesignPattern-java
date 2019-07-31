cd8a6ec4e11605271e86960ac004c6043dd1c807
ydj515 <ydj515@hanmail.net>
2019-08-01 01:30:10 +0900

    flyweight commit

---
+++ src/flyweight_pattern/Flyweight.java
@@ -0,0 +1,14 @@
+package flyweight_pattern;
+
+public class Flyweight {
+	
+	private String data;
+
+	public Flyweight(String data) {
+		this.data = data;
+	}
+
+	public String getData() {
+		return data;
+	}
+}
---
+++ src/flyweight_pattern/Main.java
@@ -0,0 +1,29 @@
+package flyweight_pattern;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		FlyweightFactory factory = new FlyweightFactory();
+
+		Flyweight flyweight = factory.getFlyweight("A");
+		System.out.println(flyweight);
+
+		Flyweight flyweight2 = factory.getFlyweight("B");
+		System.out.println(flyweight2);
+
+		Flyweight flyweight3 = factory.getFlyweight("C");
+		System.out.println(flyweight3);
+
+		Flyweight flyweight4 = factory.getFlyweight("B");
+		System.out.println(flyweight4);
+
+		Flyweight flyweight5 = factory.getFlyweight("A");
+		System.out.println(flyweight5);
+
+		Flyweight flyweight6 = factory.getFlyweight("A");
+		System.out.println(flyweight6);
+
+	}
+
+}
